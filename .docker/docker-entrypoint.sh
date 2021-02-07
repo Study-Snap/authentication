dedbf72f9ad2f091c2662347feb8adb06907afb1
SystemFiles <bensykes12@gmail.com>
2021-02-07 16:58:07 -0500

    (deploy): Typo again

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 
 echo "Waiting for environment secrets to be loaded"
 while [[-z $DB_PASS ]]; do
-  sleep(1);
+  sleep 1;
 done
 
 echo "Building authentication service"
-npm run build
+npm run build;
 
 echo "Running authentication service..."
-npm run start
+npm run start;
